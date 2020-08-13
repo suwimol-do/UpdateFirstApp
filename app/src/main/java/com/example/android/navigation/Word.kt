fbe607e129c5049458d54e164ff5780efcb2c952
NITRO 5 <Peerapatpee@hotmail.com>
2020-08-13 17:56:53 +0700

    Update First App : Add Entity

---
+++ app/src/main/java/com/example/android/navigation/Word.kt
@@ -0,0 +1,10 @@
+package com.example.android.navigation
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+//add Entity
